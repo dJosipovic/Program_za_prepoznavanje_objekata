9fc0777cd9699e9b2ac339dc037d189c9910b995
Josipovic <katarina.josipovic@hotmail.com>
2022-10-06 18:39:43 +0200

    Add project files.

---
+++ Program_za_prepoznavanje_objekata/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Program_za_prepoznavanje_objekata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.btnOtvori = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.btnDetect);
+            this.panel1.Controls.Add(this.btnOtvori);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 762);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(149, 689);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(84, 34);
+            this.btnDetect.TabIndex = 2;
+            this.btnDetect.Text = "Prepoznaj";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // btnOtvori
+            // 
+            this.btnOtvori.Location = new System.Drawing.Point(12, 689);
+            this.btnOtvori.Name = "btnOtvori";
+            this.btnOtvori.Size = new System.Drawing.Size(86, 34);
+            this.btnOtvori.TabIndex = 1;
+            this.btnOtvori.Text = "Otvori";
+            this.btnOtvori.UseVisualStyleBackColor = true;
+            this.btnOtvori.Click += new System.EventHandler(this.btnOtvori_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(227, 555);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(256, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(905, 555);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1126, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(1173, 762);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.Button btnOtvori;
+    }
+}
+
---
+++ Program_za_prepoznavanje_objekata/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alturos.Yolo;
+using Alturos.Yolo.Model;
+
+namespace Program_za_prepoznavanje_objekata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> listimage;
+        string _filename;
+        YoloWrapper yoloWrapper;
+        //OpenFileDialog OpenFileDialog = new OpenFileDialog();
+
+        private void btnOtvori_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            //if (OpenFileDialog.ShowDialog() == DialogResult.OK && OpenFileDialog.CheckPathExists)
+            //{
+            //    foreach (string File in OpenFileDialog.FileNames)
+            //    {
+            //        ((DataGridViewImageCell)dataGridView1.Rows[0].Cells[1]).Value = Image.FromFile(File);
+            //    }
+            //}
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    var files = Directory.GetFiles(fbd.SelectedPath);
+                    var table = new DataTable();
+                    table.Columns.Add("File Name");
+
+                    foreach (var file in files)
+                    {
+                        if (Regex.IsMatch(file, @".jpg|.png|.bmp|.jpeg|.JPG|.PNG|.JPEG|.BMP$"))
+                        {
+                            listimage.Add(file);
+                            table.Rows.Add(Path.GetFileNameWithoutExtension(file));
+                        }
+                    }
+                    dataGridView1.DataSource = table;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listimage = new List<string>();
+
+            var configurationDetector = new YoloConfigurationDetector();
+            var config = configurationDetector.Detect();
+            yoloWrapper = new YoloWrapper(config);
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            _filename = listimage[dataGridView1.CurrentRow.Index];
+            var image = new Bitmap(_filename);
+            pictureBox1.Image = image;         
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            var items = yoloWrapper.Detect(_filename);
+
+            DrawBorderResult(items.ToList(), _filename);
+        }
+
+        private void DrawBorderResult(List<YoloItem> items, string filename)
+        {
+            var image = Image.FromFile(filename);
+            using(var canvas = Graphics.FromImage(image))
+            {
+                foreach (var item in items)
+                {
+                    var x = item.X;
+                    var y = item.Y;
+                    var width = item.Width;
+                    var height = item.Height;
+
+                    var confidence = item.Confidence.ToString("P", CultureInfo.InvariantCulture);
+                    Random rnd = new Random();
+                    Color color = Color.FromArgb(255, rnd.Next(255), rnd.Next(255));
+                    using(var pen = new Pen(color, 3))
+                    {
+                        canvas.DrawRectangle(pen, x, y, width, height);
+                        canvas.DrawString(item.Type + " " + confidence, new Font("Arial", 5), new SolidBrush(color), x, y);
+                        canvas.Flush();
+                    }
+                }
+
+                pictureBox1.Image = image;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
